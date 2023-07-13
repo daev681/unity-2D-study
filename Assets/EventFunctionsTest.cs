0129d13010628600c503f5c10e53b5802f516189
daev681 <wow1186@naver.com>
2023-07-13 22:15:45 +0900

    [EVENT] 테스트

---
+++ Assets/EventFunctionsTest.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private void Awake() {
+        Debug.Log("Awake 실행");
+    }
+}
