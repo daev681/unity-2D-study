5ab605341d7ff3b3e4b0ac23d223dc1461d06384
daev681 <wow1186@naver.com>
2023-07-17 11:33:49 +0900

    [2D] 충돌

--- Assets/Movement2D.cs
+++ Assets/Movement2D.cs
@@ -7,6 +7,13 @@ public class Movement2D : MonoBehaviour
 
     private float moveSpead = 5.0f;
     private Vector3 moveDirection = Vector3.zero;
+    private Rigidbody2D rigid2D;
+
+    private void Awake()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+
+    }
 
 
     private void Update() {
@@ -21,9 +28,9 @@ public class Movement2D : MonoBehaviour
         float y = Input.GetAxisRaw("Vertical"); // 위아래 이동
 
    
-        moveDirection = new Vector3(x,y,0);
+       // moveDirection = new Vector3(x,y,0);
 
-        transform.position += moveDirection * moveSpead * Time.deltaTime;
+       // transform.position += moveDirection * moveSpead * Time.deltaTime;
 
         
 
@@ -33,6 +40,24 @@ public class Movement2D : MonoBehaviour
         // Time.deltaTime -> 컴퓨터 사양때문에 거리 조절 하는 ,컴퓨터 사양에따라 update 호출 횟수가 다르다
 
          //  transform.position += Vector3.right * 1 * Time.deltaTime;
+
+        // 클래스 내부에서 이동 관련 함수 작동
+        rigid2D.velocity = new Vector3(x, y, 0) * moveSpead;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision) // 충돌 일어날 경우
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)  // 충돌이 유지
+    {
+        
+    }
+    private void OnCollisionExit2D(Collision2D collision) // 충돌이 종료
+    {
+        
     }
 
 }
